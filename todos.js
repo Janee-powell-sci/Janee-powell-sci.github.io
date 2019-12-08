067294d4164acd2d0639bad60b1e0f7167030f7f
Janee-powell-sci <janee.powell@sciacademy.org>
2019-12-08 20:56:52 +0000

    op spark

---
+++ todos.js
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title> Janee's Amazing Website</title>
+    </head>
+    
+    <body>
+    <div id="all-contents"> 
+        <nav>
+
+        </nav>
+
+        <main>
+<main>
+    <div class="sidebar">
+       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSs1vZIz4_kh7uzY5bCKkN-bP062LEZkRG3Y_2Ewasae_wsZZv3yg
+    </div>
+
+    <div class="content">
+      <section class="interests">
+    <h1> Janee Powell </h1>
+    <ul>
+        <li> Photography </li>
+        <li> Theater </li>
+        <li> gaming </li>
+    </ul>
+</section>
+    </div>
+</main>
+        </main>
+    </div>
+    </body>
+</html>
